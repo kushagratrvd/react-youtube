d6987481566148fcacd82fdee9371692fb7c858d
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2024-08-16 06:08:06 +0000

    Context Api Projetc/Discussion

---
+++ 08minicontext/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import UserContextProvider from './Context/UserContextProvider'
+import Login from './Components/Login'
+import Profile from './Components/Profile'
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      Hello People Kushagra Here
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
