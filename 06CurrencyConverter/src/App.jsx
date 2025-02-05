d347395cbf922f7599749e519a49a8e0937e64e2
Kushagra <kushagratrivedi962@gmail.com>
2025-02-05 11:31:32 +0530

    setup

---
+++ 06CurrencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello Vite + React!</h1>
+      <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
+    </div>
+  )
+}
+
+export default App
