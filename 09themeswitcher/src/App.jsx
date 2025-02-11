59f60fccecde03dc743605dc661bcf4593406e46
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2025-02-11 13:03:57 +0000

    done

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,25 +1,28 @@
 
 import { useEffect, useState } from "react"
 import { ThemeProvider } from "./Contexts/Theme"
-import ThemeBtn from "./Components/Themebtn"
 import Card from "./Components/Card"
+import ThemeBtn from "./Components/Themebtn"
+
 function App() {
-  const [themecolour, setthemeMode] = useState("light")
-
-  const lightmode = ()=>{
-    setthemeMode("light")
-  }
-  const darkmode = () =>{
-    setthemeMode("dark")
+  const [thememode, setThememode] = useState("light")
+  
+  const lightmode = () => {
+    setThememode("light")
   }
 
-  useEffect( ()=>{
-    document.querySelector('html').classList.remove('light','dark')
-    document.querySelector('html').classList.add(themecolour)
-  },[themecolour]
-  )
+  const darkmode = () => {
+    setThememode("dark")
+  }
+
+  useEffect( () => {
+    const theme = document.querySelector('html').classList
+    theme.remove("light","dark")
+    theme.add(thememode)
+  }, [thememode])
+
   return (
-    <ThemeProvider value={{themecolour, lightmode,darkmode}}>
+    <ThemeProvider value={{thememode, lightmode,darkmode}}>
     <div className="flex flex-wrap min-h-screen items-center">
     <div className="w-full">
         <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
--- 09themeswitcher/src/Contexts/Theme.js
+++ 09themeswitcher/src/Contexts/Theme.js
@@ -1,14 +1,13 @@
 import { createContext, useContext } from "react";
 
-export const ThemeContext = createContext({
-    themecolour: "light",
-    lightmode : () => {},
-    darkmode : () => {},
+const ThemeContext = createContext({
+    thememode: "light",
+    lightmode: () => {},
+    darkmode: () => {}
 })
 
-export const ThemeProvider = ThemeContext.Provider
+export const ThemeProvider = ThemeContext.Provider()
 
 export default function useTheme(){
     return useContext(ThemeContext)
-    
 }
