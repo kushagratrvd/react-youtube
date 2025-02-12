e388d7f7ab443e31c1e491bb6154291fb48e5e6a
kushagratrvd <146850405+kushagratrvd@users.noreply.github.com>
2025-02-12 15:06:13 +0000

    done

--- 10todocontextLocal/src/App.jsx
+++ 10todocontextLocal/src/App.jsx
@@ -5,35 +5,31 @@ import  TodoItem  from './Components/TodoItem'
 
 
 function App() {
-
-  const [todos, setTodos] = useState([])
-
-  const addTodo = (todo)=>{
-    setTodos((prev) => [{ id: Date.now(), ...todo},...prev])
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id:Date.now() , ...todo},...prev])
   }
 
-  const updateTodo = (id,todo) => { 
-    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo: prevTodo)))
+  const updateTodo = (id,todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)) )
   }
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+    setTodos((prev) => prev.filter((Todo) => (Todo.id !== id)) )
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => 
-      prev.map((prevTodo) =>
-         prevTodo.id === id?{...prevTodo, completed: !prevTodo.completed}:prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo)) )
   }
 
   useEffect(() => {
-    const todos = JSON.parse(localStorage.getItem("todos"))
-    if (todos && todos.length > 0) {
+    const todos = JSON.parse(localStorage.getItem('todos'))
+
+    if (todos && todos.length > 0){
       setTodos(todos)
     }
-  }, [])
+  },[])
 
-  useEffect(() => {
+  useEffect( () => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
 
