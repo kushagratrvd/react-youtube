3900f0eae919eebeeaa6b05f2c5d94f1ee09b590
Kushagra <kushagratrivedi962@gmail.com>
2025-04-18 12:45:38 +0530

    Blog app files

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,5 +3,14 @@ import Footer from './Footer/Footer'
 import Container from './container/container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import Button from './Button'
+import Input from './Input'
+import RTE from './RTE'
+import Select from './Select'
+import PostForm from './post-form/Postform'
+import Login from './Login'
+import SignUp from './Signup'
+import AuthLayout from './AuthLayout'
+import PostCard from './PostCard'
 
-export {Header,Footer,Container,Logo,LogoutBtn}
+export {Header,Footer,Container,Logo,LogoutBtn,Button,Input,RTE,Select,PostForm,Login,SignUp, AuthLayout, PostCard}
